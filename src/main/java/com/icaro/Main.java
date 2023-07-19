2dc95941806d488ca8fef1ae1d02b40cef7acdaf
Leofierro <fierroleonardo@gmail.com>
2023-07-19 19:50:05 -0300

    Proyecto base

---
+++ src/main/java/com/icaro/Main.java
@@ -0,0 +1,22 @@
+package com.icaro;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println ("Hello world!");
+
+        System.out.println("Good bye");
+
+        System.out.println(12345);
+
+        System.out.println(123.456);
+
+        System.out.println('c');
+
+        System.out.println(true);
+
+        System.out.println(false);
+
+
+        }
+}
